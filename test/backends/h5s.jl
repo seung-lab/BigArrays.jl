84ad6293cfcfd680b490cc91ca30e50145abbde2
jingpengwu <jingpeng.wu@gmail.com>
2016-09-16 18:14:03 -0400

    parallel reading and writing, tested for both 3D image and 4D affinity map.

--- test/backends/h5s.jl
+++ test/backends/h5s.jl
@@ -1,6 +1,7 @@
 using BigArrays
 using BigArrays.H5sBigArrays
 
+info("\n test 3D image reading and saving...")
 H5sDir = "/tmp/test.h5sbigarray.img"
 a = rand(UInt8, 200,200,10)
 ba = H5sBigArray(H5sDir; blockSize=(256,256,32), chunkSize=(8,8,2))
@@ -11,6 +12,7 @@ rm(H5sDir; recursive=true)
 
 
 # test affinity map
+info("\n test affinity map reading and saving...")
 H5sDir = "/tmp/test.h5sbigarray.aff"
 a = rand(Float32, 200,200,10,3)
 ba = H5sBigArray(H5sDir; blockSize=(256,256,32), chunkSize=(8,8,2))
