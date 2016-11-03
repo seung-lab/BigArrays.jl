dae0e2ac55132768f3f55a6ab359563fb36533f0
jingpengwu <jingpeng.wu@gmail.com>
2016-11-03 13:32:47 -0400

    fix iterator, a big bug

--- src/iterator.jl
+++ src/iterator.jl
@@ -7,6 +7,15 @@ export BigArrayIterator
 immutable BigArrayIterator{N}
     globalRange     ::CartesianRange{CartesianIndex{N}}
     blockSize       ::NTuple{N}
+    blockIDRange    ::CartesianRange{CartesianIndex{N}}
+end
+
+function BigArrayIterator{N}( globalRange::CartesianRange{CartesianIndex{N}},
+                            blockSize::NTuple{N})
+    blockIDStart = CartesianIndex(index2blockid( globalRange.start, blockSize ))
+    blockIDStop  = CartesianIndex(index2blockid( globalRange.stop,  blockSize ))
+    blockIDRange = CartesianRange(blockIDStart, blockIDStop)
+    BigArrayIterator( globalRange, blockSize, blockIDRange )
 end
 
 function BigArrayIterator( ba::AbstractBigArray )
@@ -21,10 +30,11 @@ function Base.eltype( iter::BigArrayIterator )
     eltype( iter.globalRange )
 end
 
+"""
+the state is a tuple {blockID, and the dimension that is increasing}
+"""
 function Base.start( iter::BigArrayIterator )
-    blockID = index2blockid( iter.globalRange.start, iter.blockSize )
-    @show blockID
-    return blockID
+    iter.blockIDRange.start
 end
 
 """
@@ -32,7 +42,11 @@ end
 
 increase start coordinate following the column-order.
 """
-function Base.next{N}( iter::BigArrayIterator{N}, blockID::NTuple{N} )
+function Base.next{N}(  iter    ::BigArrayIterator{N},
+                        state   ::CartesianIndex{N} )
+    blockIDIndex, state = next(iter.blockIDRange, state)
+    blockID = tuple(blockIDIndex.I...)
+
     # get current global range in this block
     start = CartesianIndex(( map((x,y,z)->max((x-1)*y+1, z), blockID,
                             iter.blockSize, iter.globalRange.start )...))
@@ -43,28 +57,17 @@ function Base.next{N}( iter::BigArrayIterator{N}, blockID::NTuple{N} )
     blockRange  = global_range2block_range( globalRange, iter.blockSize)
     bufferRange = global_range2buffer_range(globalRange, iter.globalRange)
 
-    # find next blockID
-    for i in 1:N
-        if blockID[i]*iter.blockSize[i]+1 < iter.globalRange.stop[i]
-            newBlockID = (blockID[1:i-1]..., blockID[i]+1, blockID[i+1:end]...)
-            return (blockID, globalRange, blockRange, bufferRange), newBlockID
-        end
-    end
-    newBlockID = (blockID[1:N-1]..., blockID[N]+1)
-    return (blockID, globalRange, blockRange, bufferRange), newBlockID
+    return (blockID, globalRange, blockRange, bufferRange), state
 end
 
 """
-    Base.done( iter::BigArrayIterator,  state::CartesianRange)
+    Base.done( iter::BigArrayIterator,  state::CartesianRange )
 
 if all the axeses were saturated, stop the iteration.
 """
-function Base.done{N}( iter::BigArrayIterator{N},  blockID::NTuple{N})
-    if (blockID[N]-1)*iter.blockSize[N]+1 > iter.globalRange.stop[N]
-        return true
-    else
-        return false
-    end
+function Base.done{N}(  iter::BigArrayIterator{N},
+                        state::CartesianIndex{N})
+    done(iter.blockIDRange, state)
 end
 
 end # end of module
--- test/backends/h5s.jl
+++ test/backends/h5s.jl
@@ -5,14 +5,14 @@ info("\n test 3D image reading and saving...")
 H5sDir = "/tmp/test.h5sbigarray.img"
 a = rand(UInt8, 200,200,10)
 ba = H5sBigArray(H5sDir; blockSize=(256,256,32), chunkSize=(8,8,2))
-ba[301:500, 201:400, 101:110] = a
-b = ba[301:500, 201:400, 101:110]
+ba[201:400, 201:400, 101:110] = a
+b = ba[201:400, 201:400, 101:110]
 @assert all(a.==b)
 rm(H5sDir; recursive=true)
 
 
 # test affinity map
-info("\n test affinity map reading and saving...")
+info("\n\n test affinity map reading and saving...")
 H5sDir = "/tmp/test.h5sbigarray.aff"
 a = rand(Float32, 200,200,10,3)
 ba = H5sBigArray(H5sDir; blockSize=(256,256,32,3), chunkSize=(8,8,2,3),
@@ -20,9 +20,9 @@ ba = H5sBigArray(H5sDir; blockSize=(256,256,32,3), chunkSize=(8,8,2,3),
         globalRange = CartesianRange(
                 CartesianIndex((typemax(Int), typemax(Int), typemax(Int), typemax(Int))),
                 CartesianIndex((0,0,0,0))))
-                
-ba[301:500, 201:400, 101:110, :] = a
-b = ba[301:500, 201:400, 101:110, :]
+
+ba[201:400, 201:400, 101:110, :] = a
+b = ba[201:400, 201:400, 101:110, :]
 
 @show size(a)
 @show size(b)
