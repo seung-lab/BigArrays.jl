c227bcbe6211fce2189f95c8588c756932ef4be4
jingpengwu <jingpeng.wu@gmail.com>
2017-06-25 16:55:11 -0400

    control the concurrent requests to be 4

--- src/base.jl
+++ src/base.jl
@@ -117,6 +117,32 @@ function Base.CartesianRange{D,T,N}( ba::BigArray{D,T,N} )
     return ret
 end
 
+function do_work_setindex{D,T,N,C}( chan::Channel{Tuple}, buf::Array{T,N}, ba::BigArray{D,T,N,C}, chk::Array{T,N} )
+    for (blockID, chunkGlobalRange, globalRange, rangeInChunk, rangeInBuffer) in chan
+        println("global range of chunk: $(string(chunkGlobalRange))")
+        fill!(chk, convert(T, 0))
+        delay = 0.05
+        for t in 1:4
+            try 
+                chk[rangeInChunk] = buf[rangeInBuffer]
+                ba.kvStore[ string(chunkGlobalRange) ] = encoding( chk, C)
+                break
+            catch e
+                println("catch an error while saving in BigArray: $e")
+                @show typeof(e)
+                @show stacktrace()
+                if t==4
+                    println("rethrow the error: $e")
+                    rethrow()
+                end 
+                sleep(delay*(0.8+(0.4*rand())))
+                delay *= 10
+                println("retry for the $(t)'s time: $(string(chunkGlobalRange))")
+            end
+        end
+    end 
+end 
+
 """
     put array in RAM to a BigArray
 """
@@ -128,74 +154,74 @@ function Base.setindex!{D,T,N,C}( ba::BigArray{D,T,N,C}, buf::Array{T,N},
     idxes = colon2unitRange(buf, idxes)
     baIter = BigArrayIterator(idxes, ba.chunkSize)
     chk = Array(T, ba.chunkSize)
-    #@sync begin 
-        for (blockID, chunkGlobalRange, globalRange, rangeInChunk, rangeInBuffer) in baIter
-            #@async begin
-                println("global range of chunk: $(string(chunkGlobalRange))")
-                fill!(chk, convert(T, 0))
-                delay = 0.05
-                for t in 1:4
-                    try 
-                        chk[rangeInChunk] = buf[rangeInBuffer]
-                        ba.kvStore[ string(chunkGlobalRange) ] = encoding( chk, C)
-                        break
-                    catch e
-                        println("catch an error while saving in BigArray: $e")
-                        @show typeof(e)
-                        @show stacktrace()
-                        if t==4
-                            println("rethrow the error: $e")
-                            rethrow()
-                        end 
-                        sleep(delay*(0.8+(0.4*rand())))
-                        delay *= 10
-                        println("retry for the $(t)'s time: $(string(chunkGlobalRange))")
-                    end
-                end
-            #end 
+    @sync begin 
+        chan = Channel{Tuple}(4)
+        @async begin 
+            for iter in baIter
+                put!(chan, iter)
+                #@async begin
+               #end 
+            end
+            close(chan)
         end
-    #end 
+        for i in 1:4
+            @async do_work_setindex(chan, buf, ba, chk)
+        end
+    end 
 end 
+
+
+function do_work_getindex!{D,T,N,C}(chan::Channel{Tuple}, buf::Array, ba::BigArray{D,T,N,C})
+    for (blockId, chunkGlobalRange, globalRange, rangeInChunk, rangeInBuffer) in chan 
+        # explicit error handling to deal with EOFError
+        delay = 0.05
+        for t in 1:4
+            try 
+                println("global range of chunk: $(string(chunkGlobalRange))") 
+                v = ba.kvStore[string(chunkGlobalRange)]
+                @assert isa(v, Array)
+                chk = decoding(v, C)
+                chk = reshape(reinterpret(T, chk), ba.chunkSize)
+                buf[rangeInBuffer] = chk[rangeInChunk]
+                break 
+            catch e
+                println("catch an error while getindex in BigArray: $e")
+                if isa(e, NoSuchKeyException)
+                    println("no suck key in kvstore: $(e), will fill this block as zeros")
+                    break
+                else
+                    if isa(e, EOFError)
+                        println("get EOFError in bigarray getindex: $e")
+                    end
+                    if t==4
+                        rethrow()
+                    end
+                    sleep(delay*(0.8+(0.4*rand())))
+                    delay *= 10
+                end
+            end 
+        end
+    end
+end
 
 function Base.getindex{D,T,N,C}( ba::BigArray{D, T, N, C}, idxes::Union{UnitRange, Int}...)
     sz = map(length, idxes)
     buf = zeros(eltype(ba), sz)
 
     baIter = BigArrayIterator(idxes, ba.chunkSize, ba.offset)
-    #@sync begin
-        for (blockID, chunkGlobalRange, globalRange, rangeInChunk, rangeInBuffer) in baIter
-     #       @async begin
-                # explicit error handling to deal with EOFError
-                delay = 0.05
-                for t in 1:4
-                    try 
-                        println("global range of chunk: $(string(chunkGlobalRange))") 
-                        v = ba.kvStore[string(chunkGlobalRange)]
-                        @assert isa(v, Array)
-                        chk = decoding(v, C)
-                        chk = reshape(reinterpret(T, chk), ba.chunkSize)
-                        buf[rangeInBuffer] = chk[rangeInChunk]
-                        break 
-                    catch e
-                        println("catch an error while getindex in BigArray: $e")
-                        if isa(e, NoSuchKeyException)
-                            println("no suck key in kvstore: $(e), will fill this block as zeros")
-                            break
-                        else
-                            if isa(e, EOFError)
-                                println("get EOFError in bigarray getindex: $e")
-                            end
-                            if t==4
-                                rethrow()
-                            end
-                            sleep(delay*(0.8+(0.4*rand())))
-                            delay *= 10
-                        end
-                    end 
-                end
-     #       end 
+    @sync begin
+        chan = Channel{Tuple}(4)
+        @async begin
+            for iter in baIter
+                put!(chan, iter)
+            end
+            close(chan)
         end
-    #end 
+        # control the number of concurrent requests here
+        for i in 1:4
+            @async do_work_getindex!(chan, buf, ba)
+        end
+    end 
     # handle single element indexing, return the single value
     if length(buf) == 1
         return buf[1]
