ecfb93ca821ee7741600bbab6b874cc2f61a8e42
jingpengw <jingpeng.wu@gmail.com>
2018-08-22 18:23:28 -0400

    remove redundent files

--- src/backends/include.jl
+++ src/backends/include.jl
@@ -1,5 +1,8 @@
 #include("BackendBase.jl"); using .BackendBase
 #include("H5sBigArrays.jl"); using .H5sBigArrays;
-include("BinDicts.jl"); #using .BinDicts;
-include("GSDicts.jl"); 
-include("S3Dicts.jl")
+include("BinDicts.jl"); using .BinDicts;
+
+# we can use goofys to mount s3 and google cloud storage 
+# and then many cloud storage could be supported by BinDicts automatically!
+include("GSDicts.jl"); using GSDicts;
+include("S3Dicts.jl"); using S3Dicts;
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 
 using BigArrays
+
 include("Indexes.jl")
-#include("Chunks.jl")
-#include("H5sBigArrays.jl")
 include("BinDicts.jl")
 include("backends/S3Dicts.jl")
 include("backends/GSDicts.jl")
